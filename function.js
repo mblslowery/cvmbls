ab0be2d6fe4b63183375fe421c092923fef1bf0e
mblslowery <123251578+mblslowery@users.noreply.github.com>
2023-01-22 21:44:55 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+/**document.getElementById('boton').onclick = function () {
+console.log ("Whatsapp");
+}
+*/
+
+document.getElementById('boton').addEventListener('click', function() {
+    console.log ("Whatsapp");
+    })
+    
